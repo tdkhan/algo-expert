7ecd64a59e818a7ef529b18da91d9bb3dbcebc71
tdkhan <taimurz.khan.16@gmail.com>
2022-02-13 17:57:51 -0600

    Four Number Sum

---
+++ Arrays/FourNumberSum.cpp
@@ -0,0 +1,62 @@
+/* ####################################################################################################
+
+Write a function that takes in a non-empty array of distinct integers and an integer representing a
+target sum. The function should find all quadruplets in the array that sum up to the target sum and return
+a two-dimensional array of all these quadruplets in no particular order.
+
+If no four number sum up to the target sum, the function should return an empty array.
+
+##################################################################################################### */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> fourNumberSum(vector<int> array, int targetSum) {
+	
+
+	// Solution 1: Brute force approach - O(n^4) time | o(n) space
+	vector<vector<int>> ans;
+	int size = array.size();
+	for (int i = 0; i < size-3; i++){
+		for (int j = i+1; j < size-2; j++){
+			for (int k = j+1; k < size-1; k++){
+				for (int l = k+1; l < size; l++){
+					if (array[i]+array[j]+array[k]+array[l] == targetSum)
+						ans.push_back({array[i], array[j], array[k], array[l]});
+				}
+			}
+		}
+	}
+	return ans;
+}
+
+void printVector(vector<vector<int>> array) {
+	if (array.empty() == 1){
+		cout << "[ ]" << endl;
+		return;
+	}
+	int rows = array.size();
+	int cols = array[0].size();
+	for (int i = 0; i < rows; i++){
+		cout << "["; 
+		for (int j = 0; j < cols; j++){
+			cout << array[i][j] << ", ";
+		}
+		cout << "\b\b]" << endl;
+	}
+}
+
+int main() {
+
+	// input array: [7, 6, 4, -1, 1, 2]
+	vector<int> array{7, 6, 4, -1, 1, 2};
+	int targetSum = 16;
+
+	// expected output: [7, 6, 4, -1], [7, 6, 1, 2] - the quadruplets can be ordered differently
+	vector<vector<int>> ans;
+	ans = fourNumberSum(array, targetSum);
+	printVector(ans);
+	return 0;
+}
